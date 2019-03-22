d5264b72313be271dcf4042af119153ed7919eb2
cnTailor <zhuzhuowu@126.com>
2019-03-22 18:47:28 +0800

    修改edge到edge-js

--- install/install_win.js
+++ install/install_win.js
@@ -2,20 +2,20 @@ const fs = require('fs');
 
 module.exports = function(){
 	
-	if(fs.existsSync(__dirname + "/../../electron-edge")){
-		console.log("Found electron-edge!\n");
+	if(fs.existsSync(__dirname + "/../../electron-edge-js")){
+		console.log("Found electron-edge-js!\n");
 		makeEnv(true);
 	}
 	else{
 		console.warn("\nelectron-edge not found. Trying to find edge.js\n")
 
-		if(fs.existsSync(__dirname + "/../../edge")){
+		if(fs.existsSync(__dirname + "/../../edge-js")){
 			console.log("Found edge!\n");
 			makeEnv(false);
 		}
 		
 		else{
-			console.error("edge not found\n");
+			console.error("edge-js not found\n");
 			process.exit(1);
 		}
 	}
